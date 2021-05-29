cbb3c4dc4002b68dec297098264cfa8a79356498
Taufiq Rohman <taufiqrohman@outlook.com>
2021-05-29 19:48:23 +0700

    Show user detail

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <nuxt-link
+    :to="to"
+    class="bg-blue-400 rounded-full px-8 py-4 text-xs uppercase text-white"
+  >
+    <slot />
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    to: String,
+  },
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,17 @@
 <template>
-  <div class="bg-gray-100 w-full p-4">
-    <Nuxt />
+  <div class="bg-gray-100 w-full p-8">
+    <div>sidebar</div>
+    <div>
+      Header
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
