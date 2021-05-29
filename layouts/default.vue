d33078adf68cb8018b51e6f607e4e7219133a7ca
Taufiq Rohman <taufiqrohman@outlook.com>
2021-05-29 22:04:11 +0700

    Add user info

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,9 @@
   <div class="flex h-screen overflow-hidden bg-gray-100 w-full">
     <sidebar />
     <div class="flex-1 overflow-auto">
-      <div class="w-full flex flex-row justify-between h-16 p-8">
-        <h3 class="text-gray-800 font-bold text-lg">{{ pageTitle }}</h3>
+      <div class="w-full flex flex-row justify-between items-center h-16 p-8 pt-12">
+        <h3 class="text-gray-800 font-bold text-2xl">{{ pageTitle }}</h3>
+        <user-info />
       </div>
       <div class="p-8">
         <Nuxt />
@@ -15,9 +16,10 @@
 <script lang="ts">
 import Vue from "vue";
 import Sidebar from "~/parts/Sidebar.vue";
+import UserInfo from "~/parts/UserInfo.vue";
 
 export default Vue.extend({
-  components: { Sidebar },
+  components: { Sidebar, UserInfo },
   computed: {
     pageTitle: function () {
       return this.$store.state.app.title;
---
+++ parts/UserInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-row items-center justify-center">
+    <span class="text-gray-700 text-sm mr-3">Jones Ferdinand</span>
+    <img
+      src="~/assets/images/avatar.png"
+      class="h-10 w-10 rounded-full object-cover"
+      alt="User avatar"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
